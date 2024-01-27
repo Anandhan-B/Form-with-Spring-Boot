7c899a177b57798c41a8e887b4f4c61d79623729
Anandhan-B <144510508+Anandhan-B@users.noreply.github.com>
2024-01-27 11:39:28 +0530

    update

--- src/main/java/com/example/Form/Repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Form.Repository;
-
-import com.example.Form.Model.QuestionModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository {
-}
